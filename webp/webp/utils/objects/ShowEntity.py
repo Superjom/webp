d85f434e4e53450839cb9feeb0e6da62bcf13f94
superjom <superjom@gmail.com>
2013-12-05 20:06:17 +0800

    bug 33

--- webp/webp/utils/objects/ShowEntity.py
+++ webp/webp/utils/objects/ShowEntity.py
@@ -8,15 +8,16 @@ Created on 11 25, 2013
 '''
 
 class ShowEntity(object):
-    def __init__(self, de="", mo="", data=[]):
+    def __init__(self, de="", mo="", info=[]):
         self.de = de
         self.mo = mo
-        self.data = data
+        self.info = info
+        self.data = []
         self.show()
 
-    def show(self, info=[]):
-        self.navigation = info[0].split("\t")
-        for line in info[1:]:
+    def show(self):
+        self.navigation = self.info[0].split("\t")
+        for line in self.info[1:]:
             self.data.append(line.split("\t"))
 
 
