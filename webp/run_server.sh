45ee3109e69cb9fefe29a52522702589e29ace76
chunwei <chunwei@chun-pc.(none)>
2013-12-08 19:14:56 +0800

    fix run_server bug

--- webp/run_server.sh
+++ webp/run_server.sh
@@ -3,6 +3,6 @@ TOOL_ENV=/home/wdmlink/chunwei/tools/myenv/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/wdmlink/chunwei/tools/sqlite_build/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/wdmlink/chunwei/tools/mysql/lib/mysql
 
-memcached -l 0.0.0.0 8081 &
+memcached -l 0.0.0.0 -p 8081 &
 
 $TOOL_ENV/python manage.py runserver 10.48.56.63:8090
